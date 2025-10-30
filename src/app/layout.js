9d8a07eeba4ca63fddc5589da073729174376104
cyrilkennedy <kennedycyrilchimankpam@gmail.com>
2025-10-30 13:16:48 +0100

    new tag

--- src/app/layout.js
+++ src/app/layout.js
@@ -5,6 +5,7 @@ import Navbar from '@/components/Navbar';
 import Footer from '@/components/Footer';
 import Banner from '@/components/Banner';
 import InterstitialAd from '@/components/InterstitialAd';
+import Script from 'next/script';
 
 const inter = Inter({ subsets: ['latin'] });
 
@@ -41,6 +42,16 @@ export default function RootLayout({ children }) {
         <GlobalAds />
       </head>
       <body className={inter.className}>
+        {/* Google Tag Manager (noscript) - Must be immediately after <body> */}
+        <noscript>
+          <iframe 
+            src="https://www.googletagmanager.com/ns.html?id=GTM-52K4X583"
+            height="0" 
+            width="0" 
+            style={{ display: 'none', visibility: 'hidden' }}
+          />
+        </noscript>
+
         <div className="min-h-screen flex flex-col">
           <Navbar />
 
@@ -62,6 +73,21 @@ export default function RootLayout({ children }) {
 
           <Footer />
         </div>
+
+        {/* Google Tag Manager Script - Load with high priority */}
+        <Script
+          id="google-tag-manager"
+          strategy="afterInteractive"
+          dangerouslySetInnerHTML={{
+            __html: `
+              (function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+              new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+              j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+              'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+              })(window,document,'script','dataLayer','GTM-52K4X583');
+            `,
+          }}
+        />
       </body>
     </html>
   );
